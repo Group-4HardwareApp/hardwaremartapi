f3a31173d922c4fedada97f575b74fed6e85f372
meanstack4@gmail.com <meanstack4@gmail.com>
2020-10-22 12:35:54 +0530

    Write api for category

--- src/main/java/com/hardwaremartapi/FirbaseIntialize.java
+++ src/main/java/com/hardwaremartapi/FirbaseIntialize.java
@@ -4,16 +4,21 @@ import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.stereotype.Service;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+@Service
 public class FirbaseIntialize {
 	@PostConstruct
     public void initialize() {
         try {
+        	
             InputStream serviceAccount = this.getClass().getClassLoader().getResourceAsStream("./serviceAccountKey.json");
-            FirebaseOptions options = new FirebaseOptions.Builder()
+            @SuppressWarnings("deprecation")
+			FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://hardwaremartapi.firebaseio.com")
                     .setStorageBucket("hardwaremartapi.appspot.com")
