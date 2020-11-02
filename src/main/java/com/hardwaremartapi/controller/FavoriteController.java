5db7e441b472d70a2536bb4a4d1bd2ec58033f75
damascus8 <siddharthshrivastav30@gmail.com>
2020-11-02 21:48:57 +0530

    added favourite file

--- src/main/java/com/hardwaremartapi/controller/FavoriteController.java
+++ src/main/java/com/hardwaremartapi/controller/FavoriteController.java
@@ -1,10 +1,54 @@
 package com.hardwaremartapi.controller;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.hardwaremartapi.bean.Favorite;
+import com.hardwaremartapi.exception.ResourceNotFoundException;
+import com.hardwaremartapi.service.FavoriteService;
 
 @RestController
 @RequestMapping("/favorite")
 public class FavoriteController {
-  
+	@Autowired
+	FavoriteService favoriteService;
+	private Favorite favorite;
+
+	@PostMapping("/")
+	public Favorite addFavorite(@RequestBody Favorite favorite) throws Exception {
+
+		return favoriteService.addFavorite(favorite);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> removeFavorite(@PathVariable("id") String id)
+			throws InterruptedException, ExecutionException, ResourceNotFoundException {
+		Favorite favorite = favoriteService.removeFavorite(id);
+		if (favorite != null)
+			return new ResponseEntity<Favorite>(favorite, HttpStatus.OK);
+		else
+			throw new ResourceNotFoundException("Not found" + id);
+
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<List<Favorite>> getFavourite(@PathVariable String userId)
+			throws ResourceNotFoundException, InterruptedException, ExecutionException {
+		List<Favorite> favouriteList = favoriteService.getFavorite(userId);
+		if (favouriteList != null) {
+			return new ResponseEntity<List<Favorite>>(favouriteList, HttpStatus.OK);
+		} else {
+			throw new ResourceNotFoundException("Product Not Found");
+		}
+	}
 }
--- src/main/java/com/hardwaremartapi/service/FavoriteService.java
+++ src/main/java/com/hardwaremartapi/service/FavoriteService.java
@@ -1,8 +1,44 @@
 package com.hardwaremartapi.service;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import org.springframework.stereotype.Service;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.firebase.cloud.FirestoreClient;
+import com.hardwaremartapi.bean.Favorite;
+import com.hardwaremartapi.exception.ResourceNotFoundException;
 
 @Service
 public class FavoriteService {
+	Firestore firestore = FirestoreClient.getFirestore();
 
+	public Favorite addFavorite(Favorite favorite) throws IOException {
+		String favoriteId = firestore.collection("Favorite").document().getId().toString();
+		favorite.setFavoriteId(favoriteId);
+		firestore.collection("Favorite").document(favoriteId).set(favorite);
+		return favorite;
+	}
+
+	public Favorite removeFavorite(String id)
+			throws InterruptedException, ExecutionException, ResourceNotFoundException {
+
+		DocumentReference documentReference = firestore.collection("Favorite").document(id);
+		Favorite favorite = documentReference.get().get().toObject(Favorite.class);
+		if (favorite != null)
+			documentReference.delete();
+		return favorite;
+	}
+
+	public List<Favorite> getFavorite(String userId) throws InterruptedException, ExecutionException {
+		List<Favorite> list;
+		Firestore fireStore = FirestoreClient.getFirestore();
+		CollectionReference collectionReference = fireStore.collection("Favorite");
+		Query queryi = collectionReference.whereEqualTo("userId", userId);
+		list = queryi.get().get().toObjects(Favorite.class);
+		return list;
+	}
 }
